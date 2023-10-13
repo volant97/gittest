bec9c3f6768e8f31101d20722fcde7176c83a3d5
volant97 <144371827+volant97@users.noreply.github.com>
2023-10-13 15:30:24 +0900

    test1

---
+++ test.js
@@ -0,0 +1,24 @@
+function solution(s){
+    let result = true;
+    
+    let num = 0;
+    
+    s = s.toUpperCase();
+
+    for (let i = 0; i < s.length; i++) {
+        if(s[i] === "P") {
+            num++;
+        }
+        if(s[i] === "Y") {
+            num--;
+        }
+    }
+    
+    if (num === 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+    
+    return result;
+}
