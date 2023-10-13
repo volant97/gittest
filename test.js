072508b3f34e369517f40dedea13e455de20a3f3
volant97 <144371827+volant97@users.noreply.github.com>
2023-10-13 15:40:55 +0900

    오타...

--- test.js
+++ test.js
@@ -1,3 +1,35 @@
+// 실행 O
+
+function solution(s){
+    let result = true;
+    
+    let num = 0;
+    
+    s = s.toUpperCase();
+
+    for (let i = 0; i < s.length; i++) {
+        if(s[i] === "P") {
+            num++;
+        }
+        if(s[i] === "Y") {
+            num--;
+        }
+    }
+    
+    if (num === 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+    
+    return result;
+}
+
+
+
+// 실행 X
+// lenght 오타..... -> length
+
 function solution(s){
     let result = true;
 
